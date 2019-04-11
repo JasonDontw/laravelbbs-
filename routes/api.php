9475ad26b0888c48fe0f4b4f35ddc2ce72ed30d5
Jason Dong <ee13751@gmail.com>
2019-04-11 17:30:53 +0800

    新增手機簡訊API

--- routes/api.php
+++ routes/api.php
@@ -2,27 +2,19 @@
 
 use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
 $api = app('Dingo\Api\Routing\Router'); //用於註冊dingo路由
 
-$api->version('v1', function($api) {
-    $api->get('version', function() {
-        return 'this is version v1';
+$api->version('v1', function($api) {  //定義第一版本
+    $api->get('/hi', function() {      //定義當路由為http://laravel.test/api/hi時執行
+        return response('this is version v1');
     });
 });
 
-$api->version('v2', function($api) {
-    $api->get('version', function() {
-        return response('this is version v2');
-    });
+$api->version('v1', [
+    'namespace' => 'App\Http\Controllers\Api'    //我們增加了一個參數namespace，使v1版本的路由都會指向App\Http\Controllers\Api，方便我們書寫路由。
+], function($api) {     
+    // 短信验证码
+    $api->post('verificationCodes', 'VerificationCodesController@store')
+        ->name('api.verificationCodes.store');
 });
