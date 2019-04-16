71e20f7cbef8d03176911f1ac5b6ece843041fd5
Jason Dong <ee13751@gmail.com>
2019-04-16 16:42:55 +0800

    新增user update api

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Requests\Api\UserRequest;
+use App\Models\Image;
 
 class UsersController extends Controller
 {
@@ -39,6 +40,22 @@ class UsersController extends Controller
         ->setStatusCode(201);
     }
 
+    public function update(UserRequest $request)
+    {
+    $user = $this->user();
+
+    $attributes = $request->only(['name', 'email', 'introduction']);
+
+    if ($request->avatar_image_id) {
+        $image = Image::find($request->avatar_image_id);
+
+        $attributes['avatar'] = $image->path;
+    }
+    $user->update($attributes);
+
+    return $this->response->item($user, new UserTransformer());
+    }
+    
     public function me()
     {
         return $this->response->item($this->user(), new UserTransformer()); //$this->user()等同於\Auth::guard('api')->user()。我們返回的是一個單一資源，
--- app/Http/Requests/Api/UserRequest.php
+++ app/Http/Requests/Api/UserRequest.php
@@ -23,12 +23,25 @@ class UserRequest extends FormRequest
      */
     public function rules()
     {
-        return [
-            'name' => 'required|between:3,25|regex:/^[A-Za-z0-9\-\_]+$/|unique:users,name',
-            'password' => 'required|string|min:6',
-            'verification_key' => 'required|string',
-            'verification_code' => 'required|string',
-        ];
+        switch($this->method()) {
+            case 'POST':
+                return [
+                    'name' => 'between:3,25|regex:/^[A-Za-z0-9\-\_]+$/|unique:users,name',
+                    'password' => 'required|string|min:6',
+                    'verification_key' => 'required|string',
+                    'verification_code' => 'required|string',
+                ];
+                break;
+            case 'PATCH':
+                $userId = \Auth::guard('api')->id();
+                return [
+                    'name' => 'between:3,25|regex:/^[A-Za-z0-9\-\_]+$/|unique:users,name,' .$userId,
+                    'email' => 'email',
+                    'introduction' => 'max:80',
+                    'avatar_image_id' => 'exists:images,id,type,avatar,user_id,'.$userId,
+                ];
+                break;
+        }
     }
 
     public function attributes()
@@ -36,6 +49,18 @@ class UserRequest extends FormRequest
         return [
             'verification_key' => '短信驗證碼 key',
             'verification_code' => '短信驗證碼',
+            'introduction' => '個人簡介',
         ];
     }
+
+    public function messages()
+    {
+        return [
+            'name.unique' => '用戶名已被佔用，請重新填寫',
+            'name.regex' => '用戶名只支持英文、數字、橫桿和下劃線。',
+            'name.between' => '用戶名必須介於 3 - 25 個字符之間。',
+            'name.required' => '用戶名不能為空。',
+        ];
+    }
+
 }
