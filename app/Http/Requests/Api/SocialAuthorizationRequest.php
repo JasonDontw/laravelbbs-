e22f2d8878d414c2fb4067704a666f28116b365a
Jason Dong <ee13751@gmail.com>
2019-04-16 12:37:47 +0800

    第三方登入

---
+++ app/Http/Requests/Api/SocialAuthorizationRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests\Api;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class SocialAuthorizationRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        $rules = [
+            'code' => 'required_without:access_token|string', //required_without如果_任一指定的欄位不存在時，則此欄位必須有值且不為空值
+            'access_token' => 'required_without:code|string',
+        ];
+
+        if ($this->social_type == 'weixin' && !$this->code) {
+            $rules['openid']  = 'required|string';
+        }
+
+        return $rules;
+    
+    }
+}
