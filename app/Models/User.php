b7a0c266716da8d5a9a1f5276a9910784cccf951
Jason Dong <ee13751@gmail.com>
2019-04-16 13:26:48 +0800

    新增JWT

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,18 +2,17 @@
 
 namespace App\Models;
 
+use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Auth\MustVerifyEmail as MustVerifyEmailTrait;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Contracts\Auth\MustVerifyEmail as MustVerifyEmailContract;
 use Auth;
 use Spatie\Permission\Traits\HasRoles;
 
-class User extends Authenticatable implements MustVerifyEmailContract
+class User extends Authenticatable implements JWTSubject
 {
     use HasRoles;
 
-    use MustVerifyEmailTrait;
+    
 
     use Notifiable {
         notify as protected laravelNotify;
@@ -103,5 +102,15 @@ class User extends Authenticatable implements MustVerifyEmailContract
         }
 
         $this->attributes['avatar'] = $path;
+    }
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 }
